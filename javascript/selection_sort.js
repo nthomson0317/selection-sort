042e989137d5377a3c9be29aae936232ecba3bc1
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-18 13:40:14 -0400

    done

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -7,10 +7,17 @@ arr.sort(function(a,b){
 return arr
 }
 
+
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSort([3, -1, 5, 2]));
+
+  console.log("");
+
+
+  console.log("Expecting: [-900, 2, 4, 17, 36, 7895]");
+  console.log("=>", selectionSort([17, -900, 555, 2, 36, 7895, 4]));
 
   console.log("");
 
